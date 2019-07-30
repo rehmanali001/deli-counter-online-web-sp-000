9a6c03b14fe62b8a610a3d25f6d0272db37f4416
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 00:47:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 # Write your code here.
 def line(katz_deli)
+<<<<<<< HEAD
   if katz_deli.length == 0
+=======
+  if katz_deli = ""
+>>>>>>> 8a6cacb601d3e2639036ba75b5506458788cf2c4
     puts "The line is currently empty."
   else 
     line = "The line is currently:"
@@ -16,6 +20,7 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
+<<<<<<< HEAD
 def now_serving(name)
   if name.length == 0
     puts "There is nobody waiting to be served!"
@@ -24,3 +29,6 @@ def now_serving(name)
     name = name.shift
 end
 end
+=======
+#
+>>>>>>> 8a6cacb601d3e2639036ba75b5506458788cf2c4
